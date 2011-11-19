18daec825ba6df149c8e06235c5f5dd815fe5b7a
Leon Anavi <leonanavi@gmail.com>
2011-11-19 12:22:44 +0200

    Launcher icon modified.

--- moc/moc_mainwindow.cpp
+++ moc/moc_mainwindow.cpp
@@ -1,7 +1,7 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'mainwindow.h'
 **
-** Created: Sat Nov 19 10:28:33 2011
+** Created: Sat Nov 19 12:12:38 2011
 **      by: The Qt Meta Object Compiler version 62 (Qt 4.7.4)
 **
 ** WARNING! All changes made in this file will be lost!
--- moc/moc_menuwidget.cpp
+++ moc/moc_menuwidget.cpp
@@ -1,7 +1,7 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'menuwidget.h'
 **
-** Created: Sat Nov 19 10:28:36 2011
+** Created: Sat Nov 19 12:12:41 2011
 **      by: The Qt Meta Object Compiler version 62 (Qt 4.7.4)
 **
 ** WARNING! All changes made in this file will be lost!
