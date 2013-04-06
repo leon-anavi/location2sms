b36a582156bfc989f077597ef9be33dbe19689b5
Filip Kłębczyk <fklebczyk@gmail.com>
2013-04-06 11:07:48 +0200

    Added Polish translation file

---
+++ nokia/location2sms_pl.ts
@@ -0,0 +1,83 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0">
+<context>
+    <name>AboutWidget</name>
+    <message>
+        <location filename="aboutwidget.cpp" line="83"/>
+        <source>This is an open source application available under GPLv3 licence at Gitorious: https://gitorious.org/location2sms. The application depends on GPS quality that the device delivers. It is powered by Google Maps API and requires Internet connection.Position accuracy of about 20 meters can be expected. </source>
+        <translation>To jest aplikacja open source dostępna na licencji GPLv3 i hostowana na Gitorious: https://gitorious.org/location2sms. Aplikacja zależy od jakości odbiornika GPS, które urządzenie dostarcza. Aplikacja wykorzystuje API Google Maps wymaga połączenia internetowego. Oczekiwana dokładność określenia pozycji to około 20 metrów.</translation>
+    </message>
+</context>
+<context>
+    <name>LanguagesWidget</name>
+    <message>
+        <location filename="languageswidget.cpp" line="385"/>
+        <source>Language:</source>
+        <translation>Język:</translation>
+    </message>
+    <message>
+        <location filename="languageswidget.cpp" line="379"/>
+        <source>Map:</source>
+        <translation>Mapa:</translation>
+    </message>
+    <message>
+        <location filename="languageswidget.cpp" line="75"/>
+        <source>OK</source>
+        <translation>OK</translation>
+    </message>
+</context>
+<context>
+    <name>MainWidget</name>
+    <message>
+        <location filename="mainwidget.cpp" line="319"/>
+        <source>Address: </source>
+        <translation>Adres: </translation>
+    </message>
+    <message>
+        <location filename="mainwidget.cpp" line="321"/>
+        <source>Latitude: </source>
+        <translation>Szerokość: </translation>
+    </message>
+    <message>
+        <location filename="mainwidget.cpp" line="324"/>
+        <source>Longitude: </source>
+        <translation>Długość: </translation>
+    </message>
+    <message>
+        <location filename="mainwidget.cpp" line="338"/>
+        <source>Sent from </source>
+        <translation>Wysłane z </translation>
+    </message>
+    <message>
+        <location filename="mainwidget.cpp" line="369"/>
+        <source>Address </source>
+        <translation>Adres </translation>
+    </message>
+    <message>
+        <location filename="mainwidget.cpp" line="392"/>
+        <source>Latitude </source>
+        <translation>Szerokość </translation>
+    </message>
+    <message>
+        <location filename="mainwidget.cpp" line="402"/>
+        <source>Longitude </source>
+        <translation>Długość </translation>
+    </message>
+    <message>
+        <location filename="mainwidget.cpp" line="536"/>
+        <source>Please wait...</source>
+        <translation>Proszę czekać...</translation>
+    </message>
+    <message>
+        <location filename="mainwidget.cpp" line="543"/>
+        <source>SMS</source>
+        <translation>SMS</translation>
+    </message>
+    <message>
+        <location filename="mainwidget.cpp" line="549"/>
+        <source>E-mail</source>
+        <translation>E-mail</translation>
+    </message>
+</context>
+</TS>
